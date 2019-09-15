541fc0180e38994f4665665434fd110ebc79efb5
Andrei Vasilcoi <vasy1796@gmail.com>
2019-09-15 22:30:10 +0100

    Completed first module
    added walking skeleton

--- SalsaSocialize/Startup.cs
+++ SalsaSocialize/Startup.cs
@@ -27,6 +27,7 @@ namespace SalsaSocialize
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<DataContext>(x=>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -45,6 +46,7 @@ namespace SalsaSocialize
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
