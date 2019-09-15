2b2f55f6a3ae57a296830f9b392e4670cfe787d0
Andrei Vasilcoi <vasy1796@gmail.com>
2019-09-15 20:44:00 +0100

    firstCommit with scaffolding

---
+++ SalsaSocialize/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SalsaSocialize.Models;
+
+namespace SalsaSocialize.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
