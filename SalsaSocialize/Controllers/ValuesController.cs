758fcb41a4d5936c68ba8b249bdbf4dcb0edbf94
Andrei Vasilcoi <vasy1796@gmail.com>
2019-09-16 17:51:20 +0100

    completed login frontend

--- SalsaSocialize/Controllers/ValuesController.cs
+++ SalsaSocialize/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace SalsaSocialize.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
